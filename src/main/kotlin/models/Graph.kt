92de7ff4b222e19a33da9c0296f3bedf34dce4c7
Brayan157 <103609696+Brayan157@users.noreply.github.com>
2023-05-27 09:24:24 -0300

    Criação das listas adjacência e incidência

--- src/main/kotlin/models/Graph.kt
+++ src/main/kotlin/models/Graph.kt
@@ -80,7 +80,52 @@ class Graph {
             matrizIncidence[origin][column] = 1
             matrizIncidence[destiny][column] = 1
         }
+    }
+    fun listaAdjacenci():Map<Int, MutableList<Int>>{
+        val listAdjacency = mutableMapOf<Int, MutableList<Int>>()
+        val positions = mutableMapOf<Vertex, Int>()
+        for ((numero, vertex) in vertices.withIndex()){
+            positions[vertex] = numero
+        }
+        edges.forEach {
+            val origin = positions[it.vertices.first]
+            val destiny = positions[it.vertices.second]
+            if (origin != null && destiny != null){
+                listAdjacencyToAdd(listAdjacency, origin, destiny)
+            }
+        }
+        return listAdjacency
+    }
 
+    fun listAdjacencyToAdd(listAdjacency: MutableMap<Int, MutableList<Int>>, origin: Int, destiny: Int) {
+        listAdjacency.getOrPut(origin) { mutableListOf() }.add(destiny)
+        if (origin != destiny){
+            listAdjacency.getOrPut(destiny) { mutableListOf() }.add(origin)
+        }
+    }
+
+    fun listIncidence(): Map<Int, MutableList<Int>>{
+        val listIncidence = mutableMapOf<Int, MutableList<Int>>()
+        val positions = mutableMapOf<Vertex, Int>()
+        for ((numero, vertex) in vertices.withIndex()){
+            positions[vertex] = numero
+        }
+        edges.forEachIndexed { edgeIndex, edge ->
+            val origin = positions[edge.vertices.first]
+            val destiny = positions[edge.vertices.second]
+
+            if (origin != null && destiny != null) {
+                listIncidenceToAdd(listIncidence, origin, destiny, edgeIndex)
+            }
+        }
+        return listIncidence
+    }
+
+    fun listIncidenceToAdd(listIncidence: MutableMap<Int, MutableList<Int>>, origin: Int, destiny: Int, edgeIndex: Int) {
+        listIncidence.getOrPut(origin) { mutableListOf() }.add(edgeIndex)
+        if (origin != destiny){
+            listIncidence.getOrPut(destiny) { mutableListOf() }.add(edgeIndex)
+        }
     }
 
     fun getVertices() = vertices
